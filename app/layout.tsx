e95a53e2554c696ecc9cb28de779c1d59ec58e52
brad <xiaoleluo2@gmail.com>
2025-09-01 02:37:19 +1000

    Update package dependencies and integrate Google Tag Manager from @next/third-parties
    - Added @next/third-parties package to package.json and package-lock.json
    - Replaced GTM script with GoogleTagManager component for improved integration in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 import { Providers } from './providers'
 import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import Script from 'next/script'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export const metadata: Metadata = {
   title: 'Web Tools - Useful Online Tools',
@@ -18,26 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5DL4BPPS');`
-          }}
-        />
-      </head>
+      <head />
+        <GoogleTagManager gtmId="GTM-TSJZSTP" />
       <body className="bg-gray-50 dark:bg-gray-900 min-h-screen transition-colors">
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5DL4BPPS"
-            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
         <Providers>{children}</Providers>
         <Analytics />
         <SpeedInsights />
