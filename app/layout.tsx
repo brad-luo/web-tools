84cb6c72c075e70248fbe0ac9a4915289d1addf2
brad <xiaoleluo2@gmail.com>
2025-09-01 01:22:06 +1000

    Refactor Google Tag Manager integration in layout
    - Moved GTM script into the head section for better structure
    - Renamed script ID for clarity
    - Cleaned up formatting in the noscript iframe

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,32 +18,31 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Script
-        id="google-tag-manager"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5DL4BPPS');`
-        }}
-      />
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5DL4BPPS');`
+          }}
+        />
+      </head>
       <body className="bg-gray-50 dark:bg-gray-900 min-h-screen transition-colors">
         <noscript>
-          <iframe 
+          <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-5DL4BPPS"
-            height="0" 
-            width="0" 
-            style={{display:'none', visibility:'hidden'}}
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}
           />
         </noscript>
-        <Providers>
-          {children}
-        </Providers>
+        <Providers>{children}</Providers>
         <Analytics />
         <SpeedInsights />
       </body>
     </html>
+
   )
 }
